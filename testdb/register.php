35e63fd965ddbb3f618313a5af232912330fc666
unknown <muneerahmed48@yahoo.com>
2016-02-04 23:06:34 +0530

    msg

---
+++ testdb/register.php
@@ -0,0 +1,17 @@
+<?php
+	require "init.php";
+	
+	$key = $_POST["key"];
+	$value = $_POST["value"];
+	
+	$sql = "INSERT INTO Table(Key, Value) VALUES('$key', '$value');";
+	
+	if(mysqli_query($conn, $sql))
+	{
+		//echo "Inserted!";
+	}
+	else
+	{
+		//echo mysqli_error($conn);
+	}
+?>
