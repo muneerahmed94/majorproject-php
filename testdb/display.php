42de54d554ce0d9921df72d64d2ee6e5da3fd92d
unknown <muneerahmed48@yahoo.com>
2016-02-05 09:37:08 +0530

    msg

--- testdb/display.php
+++ testdb/display.php
@@ -1,17 +1,22 @@
 <?php  
-	require "init.php";  
-	$roll_no = $_GET["roll_no"];  
-   
-	$sql_query = "SELECT Name FROM Students WHERE ID = '$roll_no';";  
-	$result = mysqli_query($con, $sql_query);  
-	if(mysqli_num_rows($result) > 0)  
-	{  
-		$row = mysqli_fetch_assoc($result);  
-		$name = $row["Name"];  
+	<?php
+	require "init.php";
+	
+	$roll_no = $_POST["roll_no"];
+	
+	$sql = "SELECT Name FROM Students WHERE ID = '$roll_no';";
+	
+	$result = mysqli_query($conn, $sql);
+	
+	if(mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$name = $row["Name"];
 		echo $name;
-	}  
-	else  
-	{   
-		echo "Not found!";  
-	}  
+	}
+	else
+	{
+		echo "Not Found!";
+	}
+?>
  ?>  
