2b3dd602e47ca51469fd35155b40d13c215b29f2
unknown <muneerahmed48@yahoo.com>
2016-02-05 08:59:50 +0530

    msg

--- testdb/display.php
+++ testdb/display.php
@@ -2,7 +2,7 @@
  require "init.php";  
  $user_name = $_POST["login_name"];  
  $user_pass =  $_POST["login_pass"];  
- $sql_query = "select name from user_info where user_name like '$user_name' and user_pass like '$user_pass';";  
+ $sql_query = "select  from user_info where user_name like '$user_name' and user_pass like '$user_pass';";  
  $result = mysqli_query($con,$sql_query);  
  if(mysqli_num_rows($result) >0 )  
  {  
