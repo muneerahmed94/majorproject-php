2012a5b23e6249dac165c7d505eccc3bfd2d9ac9
unknown <muneerahmed48@yahoo.com>
2016-02-05 08:56:55 +0530

    msg

--- testdb/display.php
+++ testdb/display.php
@@ -1,21 +1,17 @@
-<?php
-	require "init.php";
-	
-	$roll_no = $_POST["roll_no"];
-	
-	
-	$sql = "SELECT Name FROM Students WHERE ID = '$roll_no';";
-	
-	$result = mysqli_query($conn, $sql);
-	
-	if(mysqli_num_rows($result) > 0)
-	{
-		$row = mysqli_fetch_assoc($result);
-		$name = $row["Name"];
-		echo $name;
-	}
-	else
-	{
-		echo "Not found!";
-	} 
-?>
+<?php  
+ require "init.php";  
+ $user_name = $_POST["login_name"];  
+ $user_pass =  $_POST["login_pass"];  
+ $sql_query = "select name from user_info where user_name like '$user_name' and user_pass like '$user_pass';";  
+ $result = mysqli_query($con,$sql_query);  
+ if(mysqli_num_rows($result) >0 )  
+ {  
+ $row = mysqli_fetch_assoc($result);  
+ $name =$row["name"];  
+ echo "Login Success..Welcome ".$name;  
+ }  
+ else  
+ {   
+ echo "Login Failed.......Try Again..";  
+ }  
+ ?>  
