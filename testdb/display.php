ae03fb3ed34745b05c1609de42e7900ffe0f2f29
unknown <muneerahmed48@yahoo.com>
2016-02-05 09:23:57 +0530

    msg

--- testdb/display.php
+++ testdb/display.php
@@ -2,12 +2,12 @@
 	require "init.php";  
 	$roll_no = $_POST["roll_no"];  
    
-	$sql_query = "SELECT Name from Students WHERE ID = '$roll_no';";  
+	$sql_query = "SELECT Name FROM Students WHERE ID = '$roll_no';";  
 	$result = mysqli_query($con, $sql_query);  
-	if(mysqli_num_rows($result) >0 )  
+	if(mysqli_num_rows($result) > 0)  
 	{  
 		$row = mysqli_fetch_assoc($result);  
-		$name = $row["name"];  
+		$name = $row["Name"];  
 		echo $name;
 	}  
 	else  
