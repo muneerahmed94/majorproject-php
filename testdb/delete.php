aba53b836fddd6b070c7e07a75585970e4ef3ec8
unknown <muneerahmed48@yahoo.com>
2016-02-08 13:02:53 +0530

    msg

---
+++ testdb/delete.php
@@ -0,0 +1,20 @@
+ <?php
+	require "init.php";
+	
+	$roll_no = $_POST["roll_no"];
+	
+	$sql = "SELECT Name FROM Students WHERE ID = '$roll_no';";
+	
+	$result = mysqli_query($conn, $sql);
+	
+	if(mysqli_num_rows($result) > 0)
+	{
+		$sql2 = "DELETE FROM Students WHERE ID = '$roll_no';""
+		mysqli_query($conn, $sql);
+		echo "Deleted Successfully";
+	}
+	else
+	{
+		echo "Not Found!";
+	}
+?>
