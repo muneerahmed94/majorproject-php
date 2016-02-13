75ef7e1bd852bc64ccbf3476150da1e5610ea187
unknown <muneerahmed48@yahoo.com>
2016-02-13 22:18:09 +0530

    msg

---
+++ register/index.php
@@ -0,0 +1,18 @@
+<?php
+	require "init.php";
+	
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	
+	$sql = "INSERT INTO Users(Name, Email, Password) VALUES('$name', '$email', , '$password');";
+	
+	if(mysqli_query($conn, $sql))
+	{
+		echo "Registration Successfull";
+	}
+	else
+	{
+		echo mysqli_error($conn);
+	}
+?>
