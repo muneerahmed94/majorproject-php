cfa9535a67fa07832db78330b63443cd03f5a6f0
unknown <muneerahmed48@yahoo.com>
2016-02-14 22:04:00 +0530

    msg

---
+++ login/FetchUserData.php
@@ -0,0 +1,26 @@
+ <?php
+	require "init.php";
+	
+	$email = $_GET["email"];
+	$password = $_GET["password"];
+	
+	$sql = "SELECT * FROM Users WHERE Email = '$email' AND Password = '$password';";
+	
+	$result = mysqli_query($conn, $sql);
+	
+	$user = array();
+	
+	if(mysqli_num_rows($result) > 0)
+	{
+		$row = mysqli_fetch_assoc($result);
+		$user["name"] = $row["name"];
+		$user["email"] = $row["email"];
+		$user["password"] = $row["password"];
+	}
+	else
+	{
+		//echo "Not Found!";
+	}
+	
+	echo json_encode($user);
+?>
