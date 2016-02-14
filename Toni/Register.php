e822258575b17601fdd4dd2e78d101ecc8846252
KARAN KAN <KARAN KAN>
2016-02-14 16:48:13 +0530

    msg

--- Toni/Register.php
+++ Toni/Register.php
@@ -1,19 +1,19 @@
 <?php
 	require "init.php";
 	
-	$id = $_POST["name"];
-	$name = $_POST["age"];
-	$name = $_POST["username"];
-	$name = $_POST["password"];
+	$name = $_POST["name"];
+	$age = $_POST["age"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
 	
 	$sql = "INSERT INTO TUsers(name, age, username, password) VALUES('$name', '$age', '$username', '$password');";
 	
 	if(mysqli_query($conn, $sql))
 	{
-		echo "Inserted!";
+		//echo "Inserted!";
 	}
 	else
 	{
-		echo mysqli_error($conn);
+		//echo mysqli_error($conn);
 	}
 ?>
