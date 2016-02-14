f5acb56c25da2e432bbdff141b3520eadf5e8bc7
KARAN KAN <KARAN KAN>
2016-02-14 15:15:00 +0530

    msg

--- Toni/FetchUserData.php
+++ Toni/FetchUserData.php
@@ -13,6 +13,7 @@
 	if(mysqli_num_rows($result) > 0)
 	{
 		$row = mysqli_fetch_assoc($result);
+		echo $row;
 		$user["name"] = $row["name"];
 		$user["age"] = $row["age"];
 		$user["username"] = $row["username"];
@@ -23,6 +24,6 @@
 	{
 		echo "Not Found!";
 	}
-	echo $user;
-	echo json_encode($user);
+	//echo $user;
+	//echo json_encode($user);
 ?>
