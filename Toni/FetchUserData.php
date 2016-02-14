e33dbe4628bc8357a4a15ae4e773199ae6ab53de
KARAN KAN <KARAN KAN>
2016-02-14 15:32:55 +0530

    msg

--- Toni/FetchUserData.php
+++ Toni/FetchUserData.php
@@ -8,17 +8,20 @@
 	
 	$result = mysqli_query($conn, $sql);
 	
+	$user = array();
 	
 	if(mysqli_num_rows($result) > 0)
 	{
 		$row = mysqli_fetch_assoc($result);
-		$name = $row["name"];
-		echo $name;
+		$user["name"] = $row["name"];
+		$user["age"] = $row["age"];
+		$user["username"] = $row["username"];
+		$user["password"] = $row["password"];
 	}
 	else
 	{
 		echo "Not Found!";
 	}
-	//echo $user;
-	//echo json_encode($user);
+	
+	echo json_encode($user);
 ?>
