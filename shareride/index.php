b1da4f36723b01e1e78eea1407300d0c4d7d356b
unknown <muneerahmed48@yahoo.com>
2016-03-06 23:15:22 +0530

    msg

--- shareride/index.php
+++ shareride/index.php
@@ -2,6 +2,8 @@
 	require "init.php";
 	
 	$email = $_POST["email"];
+	$name = $_POST["name"];
+	$mobile = $_POST["mobile"];
 	$from = $_POST["from"];
 	$to = $_POST["to"];
 	$date = $_POST["date"];
@@ -11,7 +13,7 @@
 	$noofseats = $_POST["noofseats"];
 	$price = $_POST["price"];
 	
-	$sql = "INSERT INTO `majorproject`.`Rides` (`RideID`, `Driver`, `From`, `To`, `Date`, `Time`, `CarNo`, `CarName`, `NoOfSeats`, `Price`) VALUES (NULL, '$email', '$from', '$to', '$date', '$time', '$carno', '$carname', '$noofseats', '$price');";
+	$sql = "INSERT INTO `majorproject`.`Rides` (`RideID`, `DriverName`, `DriverEmail`, `DriverMobile`,`From`, `To`, `Date`, `Time`, `CarNo`, `CarName`, `NoOfSeats`, `Price`) VALUES (NULL, '$name', '$email', '$mobile', '$from', '$to', '$date', '$time', '$carno', '$carname', '$noofseats', '$price');";
 	
 	if(mysqli_query($conn, $sql))
 	{
