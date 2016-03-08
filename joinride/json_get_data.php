2dd1f02694fa19da365542ff711c1551c31cc29c
unknown <muneerahmed48@yahoo.com>
2016-03-08 07:29:28 +0530

    msg

--- joinride/json_get_data.php
+++ joinride/json_get_data.php
@@ -9,7 +9,7 @@
 	
 	while($row = mysqli_fetch_array($result))
 	{
-		array_push($response, array("RideID"=>$row[0], "Driver"=>$row[1], "From"=>$row[2], "To"=>$row[3], "Date"=>$row[4], "Time"=>$row[5], "CarNo"=>$row[6], "CarName"=>$row[7], "NoOfSeats"=>$row[8], "Price"=>$row[9]));
+		array_push($response, array("RideID"=>$row[0], "DriverName"=>$row[1], "DriverEmail"=>$row[2], "DriverMobile"=>$row[3],"From"=>$row[4], "To"=>$row[5], "Date"=>$row[6], "Time"=>$row[7], "CarNo"=>$row[8], "CarName"=>$row[9], "NoOfSeats"=>$row[10], "Price"=>$row[11]));
 	}
 	
 	echo json_encode(array("server_response"=>$response));
