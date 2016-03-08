88d65a6066cc9fe5aac629a7b6b36a00767e44e6
unknown <muneerahmed48@yahoo.com>
2016-03-08 11:36:49 +0530

    msg

--- joinride/json_get_data.php
+++ joinride/json_get_data.php
@@ -1,7 +1,7 @@
  <?php
 	require "init.php";
 	
-	$sql = "SELECT * FROM Rides WHERE NoOfSeats > 0";
+	$sql = "SELECT * FROM Rides WHERE Date > CURDATE() && TIME > CURTIME() && NoOfSeats > 0";
 	
 	$result = mysqli_query($conn, $sql);
 	
