d99ed5c77da0a673b37129d6db487bb8e28c7dd9
unknown <muneerahmed48@yahoo.com>
2016-03-13 14:52:25 +0530

    msg

---
+++ myride/init.php
@@ -0,0 +1,24 @@
+<?php
+	define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+	define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+	define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+	define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+	define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+
+	$db_host = constant("DB_HOST"); // Host name 
+	$db_port = constant("DB_PORT"); // Host port
+	$db_username = constant("DB_USER"); // Mysql username 
+	$db_password = constant("DB_PASS"); // Mysql password 
+	$db_name = constant("DB_NAME"); // Database name 
+	
+	$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+	
+	if(!$conn)
+	{
+		echo "Connection failed...".mysqli_connect_error();
+	}
+	else
+	{
+		//echo "Connected!";
+	}
+?>
