6aabb9abc8562f7197f9fe671f3e03e172c0f171
unknown <muneerahmed48@yahoo.com>
2016-03-17 09:09:21 +0530

    msg

--- shareride/index.php
+++ shareride/index.php
@@ -19,6 +19,19 @@
 	{
 		echo "Ride Shared!";
 		
+		$sql2 = "SELECT * FROM Rides WHERE DriverName = '$name' AND DriverEmail = '$email' AND DriverMobile = '$mobile' AND From = '$from' AND To = '$to' AND Date = '$date' AND Time = '$time' AND CarNo = '$carno' AND CarName = '$carname' AND NoOfSeats = '$noofseats' AND Price = '$price';";
+		$result = mysqli_query($conn, $sql2);
+		$ride_id;
+		if(mysqli_num_rows($result) > 0)
+		{
+			$row = mysqli_fetch_assoc($result);
+			$ride_id = $row["RideID`"];
+		}
+		else
+		{
+			//echo "Not Found!";
+		}
+		
 		$sql3 = "INSERT INTO `majorproject`.`JoinedRides` (`JoinedRideID`, `RideID`, `PassengerName`, `PassengerEmail`, `PassengerMobile`, `NoOfSeatsBooked`, `DriverName`, `DriverEmail`, `DriverMobile`, `From`, `To`, `Date`, `Time`, `CarNo`, `CarName`, `NoOfSeats`, `Price`) VALUES (NULL, NULL, '$name', '$email', '$mobile', NULL, '$name', '$email', '$mobile', '$from', '$to', '$date', '$time', '$carno', '$carname', '$noofseats', '$price');";
 		if(mysqli_query($conn, $sql3))
 		{
