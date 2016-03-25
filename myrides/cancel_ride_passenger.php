728c80700711b0a606c520f72026c9d24de8bc2d
unknown <muneerahmed48@yahoo.com>
2016-03-25 23:09:12 +0530

    msg

--- myrides/cancel_ride_passenger.php
+++ myrides/cancel_ride_passenger.php
@@ -2,22 +2,16 @@
 	require "init.php";
 	
 	$RideID = $_POST["RideID"];
+	$NoOfSeatsNew = $_POST["NoOfSeatsNew"];
+	$PassengerEmail = $_POST["PassengerEmail"];
 	
-	$sql = "SELECT * FROM `JoinedRides` WHERE `RideID` = '$RideID' AND `DriverEmail` NOT LIKE `PassengerEmail`;";
-	
+	//update the NoOfSeatsNew (available) in Rides table
+	$sql = "UPDATE `Rides` SET `NoOfSeats` = '$NoOfSeats' WHERE `RideID` = '$RideID';";
 	$result = mysqli_query($conn, $sql);
 	
-	$response = array();
-	
-	while($row = mysqli_fetch_array($result))
-	{
-		array_push($response, array("JoinedRideID"=>$row[0], "RideID"=>$row[1], "PassengerName"=>$row[2], "PassengerEmail"=>$row[3],  "PassengerMobile"=>$row[4], "NoOfSeatsBooked"=>$row[5], "DriverName"=>$row[6], "DriverEmail"=>$row[7], "DriverMobile"=>$row[8], "From"=>$row[9], "To"=>$row[10], "Date"=>$row[11], "Time"=>$row[12], "CarNo"=>$row[13], "CarName"=>$row[14], "NoOfSeats"=>$row[15], "Price"=>$row[16]));
-	}
-	
-	$sql1 = "UPDATE `JoinedRides` SET `Status` = 'complete' WHERE `RideID` = '$RideID';";
+	//delete the corresponding ride from the JoinedRides
+	$sql1 = "DELETE FROM `JoinedRides` WHERE `RideID` = '$RideID' AND `PassengerEmail` = '$PassengerEmail';";
 	mysqli_query($conn, $sql1);
-	
-	echo json_encode(array("server_response"=>$response));
-	
+		
 	mysqli_close($conn);
 ?>
