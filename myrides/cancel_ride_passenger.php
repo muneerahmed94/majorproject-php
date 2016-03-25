de8b975172d4cdcc20ec8e53c291cbc66bb44ea2
unknown <muneerahmed48@yahoo.com>
2016-03-25 23:12:12 +0530

    msg

--- myrides/cancel_ride_passenger.php
+++ myrides/cancel_ride_passenger.php
@@ -1,9 +1,9 @@
  <?php
 	require "init.php";
 	
-	$RideID = $_POST["RideID"];
-	$NoOfSeatsNew = $_POST["NoOfSeatsNew"];
-	$PassengerEmail = $_POST["PassengerEmail"];
+	$RideID = $_GET["RideID"];
+	$NoOfSeatsNew = $_GET["NoOfSeatsNew"];
+	$PassengerEmail = $_GET["PassengerEmail"];
 	
 	//update the NoOfSeatsNew (available) in Rides table
 	$sql = "UPDATE `Rides` SET `NoOfSeats` = '$NoOfSeats' WHERE `RideID` = '$RideID';";
