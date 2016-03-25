c314c6d458da05bc6d8bed4172f91fede863826a
unknown <muneerahmed48@yahoo.com>
2016-03-25 23:25:31 +0530

    msg

--- myrides/cancel_ride_passenger.php
+++ myrides/cancel_ride_passenger.php
@@ -12,6 +12,10 @@
 	//delete the corresponding ride from the JoinedRides
 	$sql1 = "DELETE FROM `JoinedRides` WHERE `RideID` = '$RideID' AND `PassengerEmail` = '$PassengerEmail';";
 	mysqli_query($conn, $sql1);
+	
+	//update the NoOfSeats in JoinedRides table
+	$sql = "UPDATE `JoinedRides` SET `NoOfSeats` = '$NoOfSeatsNew' WHERE `RideID` = '$RideID';";
+	$result = mysqli_query($conn, $sql);
 		
 	mysqli_close($conn);
 ?>
